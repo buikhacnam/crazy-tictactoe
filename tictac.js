b19d5e5d5c24f8ef898d5f6612b2e26060094aed
Bui Khac Nam <buikhacnam11@gmail.com>
2020-09-04 13:24:39 +0700

    weqwe

--- tictac.js
+++ tictac.js
@@ -1,3 +1,5 @@
+const mode = document.getElementById("modes");
+
 const num1 = document.getElementById("1");
 const num2 = document.getElementById("2");
 const num3 = document.getElementById("3");
@@ -8,12 +10,60 @@ const num7 = document.getElementById("7");
 const num8 = document.getElementById("8");
 const num9 = document.getElementById("9");
 
-const twoPlayer = true;
+const o = document.getElementById("o");
+const x = document.getElementById("x");
+
+const status = document.getElementById("status");
+
+const restart = document.getElementById("re-start");
+
+let twoPlayer = false;
+
+
 let numTurn = 0;
 
-const player1 = [];
-const player2 = [];
-const computer = [];
+let player1 = [];
+let player2 = [];
+let computer = [];
+
+mode.addEventListener("change", gamePlan);
+function gamePlan() {
+ 
+	if(mode.value == "two-players"){
+		stop2();
+		console.log("two player")
+		 twoPlayer = true;
+	} else if(mode.value == "easy") {
+		stop();
+		console.log("one player")
+	 	twoPlayer = false;
+		
+	}
+	newGame();
+ }
+
+
+
+restart.addEventListener("click", newGame);
+
+function newGame() {
+		player1 = [];
+		player2 = [];
+		computer = [];
+		numTurn = 0;
+		turnOfcomputer = 0;
+		turnOfplayer = 0;
+	return pick.forEach(num => {	
+		
+		num.innerHTML = "";
+		if ((twoPlayer)) {	
+			num.addEventListener("click", display);
+		} else {
+			num.addEventListener("click",display2);
+		} 
+	})
+}
+
 
 const winArray = [[1,2,3],[1,4,7],[7,8,9],[9,6,3],[1,5,9],[7,5,3],[4,5,6],[2,5,8]];
 
@@ -21,13 +71,13 @@ const pick = [num1, num2, num3, num4, num5, num6, num7, num8, num9];
 
 
 pick.forEach(num => {	
-	if (twoPlayer) {	
+	if ((twoPlayer)) {	
 		num.addEventListener("click", display);
 	} else {
 		num.addEventListener("click",display2);
 	}
 
-})
+}) 
 
 
 function check(player) {
@@ -42,24 +92,31 @@ function check(player) {
 function display(e) {
 	numTurn += 1;
 	if (numTurn % 2 == 0) { 
+		o.style.backgroundColor = "#53c653";
+		x.style.backgroundColor = "white";
+		status.innerHTML = "<i class='far fa-circle'></i> turn";
 		e.target.innerHTML =  "<h1><i class='fas fa-times'></i></h1>";
 		player1.push(Number.parseInt((e.target.id), 10));
 		if (check(player1)) {
-			console.log("X won");
+			status.innerHTML = "<i class='fas fa-times'></i> WINS !!!";
 			stop();
 		} else if (!check(player1) && numTurn == 9) {
-			console.log("Draw");
+			status.innerHTML=("Draw");
 			stop();
 		}
 	
 	} else {
+		x.style.backgroundColor = "#53c653";
+		o.style.backgroundColor = "white";
+		status.innerHTML = "<i class='fas fa-times'></i> turn";
 		e.target.innerHTML = "<h1><i class='far fa-circle'></i></h1>";
+
 		player2.push(Number.parseInt((e.target.id), 10));
 		if (check(player2)) {
-			console.log("O won");
+			status.innerHTML = "<i class='far fa-circle'></i> WINS !!!";
 			stop();
 		} else if (!check(player2) && numTurn == 9) {
-			console.log("Draw");
+			status.innerHTML=("Draw");
 			stop();
 		}
 	}
@@ -96,10 +153,13 @@ let turnOfplayer = 0;
 function display2(e) {	
     if (turnOfplayer == turnOfcomputer) {
     	turnOfplayer += 1;	
-		e.target.innerHTML = "O";
+		x.style.backgroundColor = "#53c653";
+		o.style.backgroundColor = "white";
+		status.innerHTML = "<i class='fas fa-times'></i> turn";
+		e.target.innerHTML = "<h1><i class='far fa-circle'></i></h1>";
 		player2.push(Number.parseInt((e.target.id), 10));
 		if (check(player2)) {
-			console.log("O won");
+			status.innerHTML = "<i class='far fa-circle'></i> WINS !!!";
 			stop2();
 						
 		} else {
@@ -107,7 +167,7 @@ function display2(e) {
 			computerTurn();
 			} else {
 				if (!check(player2)) {
-					console.log("draw");
+					status.innerHTML=("Draw");
 				}
 			}	
    		}
@@ -117,19 +177,23 @@ function display2(e) {
 
 function computerTurn() {
     setTimeout( () => {
-    	let location;
+		o.style.backgroundColor = "#53c653";
+		x.style.backgroundColor = "white";
+		status.innerHTML = "<i class='far fa-circle'></i> turn";
+		let location;
     	do {
     		location = autoGenerate();       
     	} while ((player2.includes(location + 1)) || (computer.includes(location)));
     	computer.push(location);
     	pick[location].removeEventListener("click", display2);
-		pick[location].innerHTML = "X";
+		pick[location].innerHTML = "<h1><i class='fas fa-times'></i></h1>";
 	
 		if (check2(computer)) {
-			console.log("X won");
+			status.innerHTML = "<i class='fas fa-times'></i> WINS !!!";
 			stop2();
 		}   
-   		turnOfcomputer += 1;
+		   turnOfcomputer += 1;
+		
     }, 1000)    
 }
 
@@ -145,3 +209,4 @@ function stop2() {
 }
 
 
+console.log(twoPlayer);
